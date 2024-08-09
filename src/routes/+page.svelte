94a8225b7cba08dccbf05b1ebeb800aee1b4739a
Nachop51 <nachoperalta0@gmail.com>
2024-08-09 16:12:49 -0300

    add base pages and setups

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,35 @@
-<h1 class="text-7xl font-bold">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main class="set-width">
+	<h1 class="heading-1 my-8">Welcome to the Multipurpose Tool Hub!</h1>
+
+	<section>
+		<h2><a href="/colors">Conversions</a></h2>
+		<ul>
+			<li><a href="#color-conversion">Color Conversion</a></li>
+			<li><a href="#length-conversion">Length Conversion</a></li>
+			<li><a href="#number-conversion">Number Conversion</a></li>
+		</ul>
+	</section>
+
+	<section>
+		<h2>Numbers</h2>
+		<ul>
+			<li><a href="#base-converter">All Bases Converter</a></li>
+			<li><a href="#average-calculator">Average Calculator</a></li>
+			<li><a href="#percentage-calculator">Percentage Calculator</a></li>
+			<li><a href="#fraction-calculator">Fraction Calculator</a></li>
+		</ul>
+	</section>
+
+	<section>
+		<h2>Strings</h2>
+		<ul>
+			<li><a href="#character-counter">Character Counter</a></li>
+			<li><a href="#rot13">ROT13 Encoder/Decoder</a></li>
+			<li><a href="#case-conversion">String Case Conversion</a></li>
+			<li><a href="#compare-strings">Compare Strings</a></li>
+		</ul>
+	</section>
+</main>
 
 <style lang="postcss">
 	:global(html) {
