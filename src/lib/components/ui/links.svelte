5e3b1ab089608690e7d54ede9b989ff6f576db79
Nachop51 <nachoperalta0@gmail.com>
2024-08-10 16:13:48 -0300

    page layouts

---
+++ src/lib/components/ui/links.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { page } from '$app/stores'
+
+	export let links: { [key: string]: string } = {}
+</script>
+
+<ul>
+	{#each Object.keys(links) as link}
+		<li>
+			<a
+				href={links[link]}
+				class="link-hover link"
+				class:link-primary={$page.url.pathname === links[link]}
+			>
+				{link}
+			</a>
+		</li>
+	{/each}
+</ul>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
+	import Navbar from '$lib/components/navbar.svelte'
 	import '../app.css'
 </script>
 
+<svelte:head>
+	<title>Tools</title>
+</svelte:head>
+
 <div class="bg-gradient transition-colors" />
+<Navbar />
+
 <slot />
